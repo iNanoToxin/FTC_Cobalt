e5d95051f3fabb555e6e301ae2f96d915a0e634e
iNanoToxin <dylanleodaniel@gmail.com>
2023-02-01 16:54:21 -0500

    c

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Modes/AutonomousOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Modes/AutonomousOp.java
@@ -18,19 +18,23 @@ public class AutonomousOp extends LinearOpMode {
 		// motors.drive(1);
 		waitForStart();
 
-// starting from the right side
+		// starting from the right side
 
 		//moves to the left (change this depending on which side we are on)
 		motors.setTarget(36.5, motorMap.Direction.LEFT);
+		
+		telemetry.addData("target", motors.frontLeft.getTargetPosition());
+		telemetry.update();
+
 		sleep(1000);
 
 
-		//moves forward and the linear slide up at the same time; stops a few inches away from the junction
-		motors.setTarget(32, motorMap.Direction.FORWARD);
-		motors.linearSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-		motors.linearSlide.setTargetPosition(robotMath.linearSlideTicks(33));
-		motors.linearSlide.setPower(0.75);
-		sleep(5000);
+		// //moves forward and the linear slide up at the same time; stops a few inches away from the junction
+		// motors.setTarget(32, motorMap.Direction.FORWARD);
+		// motors.linearSlide.setTargetPosition(robotMath.linearSlideTicks(33));
+		// motors.linearSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+		// motors.linearSlide.setPower(0.75);
+		// sleep(5000);
 
 		//moves the robot forward 3 inches once the linear slide is all the way up
 		motors.setTarget(3, motorMap.Direction.FORWARD);
@@ -40,22 +44,22 @@ public class AutonomousOp extends LinearOpMode {
 		motors.setPower(1);
 		sleep(1000);
 
-/*
-		//moves backward and turns 180 degrees to face the substation while moving the linear slide back down and parks
-		motors.setTarget(23, BACKWARD);
-		motors.linearSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-		motors.linearSlide.setTargetPosition(robotMath.linearSlideTicks(33));
-		motors.linearSlide.setPower(-0.75);
-		motors.setTarget(19, ROTATE_C);
-		sleep(10000);
-	*/
+		/*
+			//moves backward and turns 180 degrees to face the substation while moving the linear slide back down and parks
+			motors.setTarget(23, BACKWARD);
+			motors.linearSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+			motors.linearSlide.setTargetPosition(robotMath.linearSlideTicks(33));
+			motors.linearSlide.setPower(-0.75);
+			motors.setTarget(19, ROTATE_C);
+			sleep(10000);
+		*/
 
 		motors.setTarget(12, motorMap.Direction.RIGHT);
 		sleep(1000);
 
 		motors.setTarget(9, motorMap.Direction.FORWARD);
-		motors.linearSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 		motors.linearSlide.setTargetPosition(robotMath.linearSlideTicks(10));
+		motors.linearSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 		motors.linearSlide.setPower(-0.75);
 		sleep(1000);
 
@@ -69,11 +73,11 @@ public class AutonomousOp extends LinearOpMode {
 		motors.setTarget(3, motorMap.Direction.FORWARD);
 		sleep(1000);
 
-		motors.linearSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-		motors.linearSlide.setTargetPosition(robotMath.linearSlideTicks(23));
-		motors.linearSlide.setPower(0.75);
-		motors.setTarget(18, motorMap.Direction.ROTATE_CC);
-		sleep(5000);
+		// motors.linearSlide.setTargetPosition(robotMath.linearSlideTicks(23));
+		// motors.linearSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+		// motors.linearSlide.setPower(0.75);
+		// motors.setTarget(18, motorMap.Direction.ROTATE_CC);
+		// sleep(5000);
 
 		motors.setTarget(6, motorMap.Direction.RIGHT);
 		motors.setTarget(30, motorMap.Direction.FORWARD);
@@ -81,37 +85,37 @@ public class AutonomousOp extends LinearOpMode {
 
 
 
-/*
-		motors.rotate(-0.8);
-		sleep(200);
-		motors.forward(0.8);
-		sleep(1000);
+		/*
+			motors.rotate(-0.8);
+			sleep(200);
+			motors.forward(0.8);
+			sleep(1000);
 
 
-		sleep(2000);
+			sleep(2000);
 
-		motors.forward(0.2);
-		sleep(100);
+			motors.forward(0.2);
+			sleep(100);
 
-		motors.claw.setPower(0.75);
-		sleep(1000);
+			motors.claw.setPower(0.75);
+			sleep(1000);
 
-		motors.backward(0.2);
-		sleep(100);
+			motors.backward(0.2);
+			sleep(100);
 
-		motors.rotate(0.8);
-		sleep(200);
+			motors.rotate(0.8);
+			sleep(200);
 
-		motors.linearSlide.setPower(0.75);
-		sleep(2000);
+			motors.linearSlide.setPower(0.75);
+			sleep(2000);
 
-		motors.forward.setPower(0.8);
-		sleep(1000);
+			motors.forward.setPower(0.8);
+			sleep(1000);
 
 
-		motors.claw.setPower
-		// while (motors.linearSlide.isBusy()) {}
-		// motors.linearSlide.setPower(0.0);
+			motors.claw.setPower
+			// while (motors.linearSlide.isBusy()) {}
+			// motors.linearSlide.setPower(0.0);
 		*/
 	}
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Modes/TelemetryOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Modes/TelemetryOp.java
@@ -18,189 +18,190 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 @TeleOp(name = "Teleop (Cobalt)")
 public class TelemetryOp extends OpMode {
-    // GoBILDA 5202/3/4: {0: backLeft, 1: frontLeft, 2: frontRight, 3: backRight}
+	// GoBILDA 5202/3/4: {0: backLeft, 1: frontLeft, 2: frontRight, 3: backRight}
 
-    // Declare OpMode members. These map motors such as frontLeft, frontRight, backLeft, backRight, etc.
-    private motorMap motors;
+	// Declare OpMode members. These map motors such as frontLeft, frontRight, backLeft, backRight, etc.
+	private motorMap motors;
 
-    // Declare our gamepad constants.
-    private final double drive_power_increment    = 0.2;
-    private final double drive_min_power          = 0.1;
-    private final double joystick_max_drift       = 0.1;
-    
-    // Declare our robot constants
-    public double claw_max_range           = 0.45;
-    public double claw_min_range           = 0.20;
-    private double drive_power                    = 0.50;
+	// Declare our gamepad constants.
+	private final double drive_power_increment	= 0.2;
+	private final double drive_min_power		  = 0.1;
+	private final double joystick_max_drift	   = 0.1;
+	
+	// Declare our robot constants
+	public double claw_max_range		   = 0.25;
+	public double claw_min_range		   = 0.00;
+	private double drive_power					= 0.50;
 
-    // Declare our boolean variables.
-    private boolean right_trigger_pressed   = false;
-    private boolean left_trigger_pressed    = false;
-    private boolean is_buttonA_pressed      = false;
-    private boolean is_claw_toggled         = false;
+	// Declare our boolean variables.
+	private boolean right_trigger_pressed   = false;
+	private boolean left_trigger_pressed	= false;
+	private boolean is_buttonA_pressed	  = false;
+	private boolean is_claw_toggled		 = false;
 
-    private final double wheel_diameter = 3.625; // calculate this
-    private final double ticks_per_rotation = 537.7;
-    private final double ticks_per_inch = Math.PI * wheel_diameter / ticks_per_rotation;
+	private final double wheel_diameter = 3.625; // calculate this
+	private final double ticks_per_rotation = 537.7;
+	private final double ticks_per_inch = Math.PI * wheel_diameter / ticks_per_rotation;
 
-    private BNO055IMU imu;
-    private PIDController pidController;
-    private ElapsedTime elapsedTime;
+	private BNO055IMU imu;
+	private PIDController pidController;
+	private ElapsedTime elapsedTime;
 
-    @Override
-    public void init() {
-        // Initializes the motors.
-        {   
-            // Maps motors to variables.
-            motors = new motorMap(hardwareMap, false);
+	@Override
+	public void init() {
+		// Initializes the motors.
+		{   
+			// Maps motors to variables.
+			motors = new motorMap(hardwareMap, false);
 
-            // Reverse the right motors.
-            motors.frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
-            motors.backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+			// Reverse the right motors.
+			motors.frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+			motors.backRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
-            // Sets direction of servo to forward.
-            // motors.claw.setDirection(Servo.Direction.FORWARD);
-            // motors.claw.setPosition(claw_max_range);
-        }
+			// Sets direction of servo to forward.
+			// motors.claw.setDirection(Servo.Direction.FORWARD);
+			motors.claw.setPosition(claw_min_range);
+		}
 
-        // Initialize and create new PID controller.
-        {
-            // Create new Proportional-Integral-Derivative Controller.
-            pidController = new PIDController(0.0, 0.8, 0.001);
-            pidController.setSetpoint(0.0);
+		// Initialize and create new PID controller.
+		{
+			// Create new Proportional-Integral-Derivative Controller.
+			pidController = new PIDController(0.0, 0.8, 0.001);
+			pidController.setSetpoint(0.0);
 
-            elapsedTime = new ElapsedTime();
+			elapsedTime = new ElapsedTime();
 
-            // initialize IMU
-            BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-            parameters.mode                 = BNO055IMU.SensorMode.IMU;
-            parameters.angleUnit            = BNO055IMU.AngleUnit.RADIANS;
-            parameters.accelUnit            = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-            parameters.loggingEnabled       = false;
-            imu                             = hardwareMap.get(BNO055IMU.class, "imu");
-            imu.initialize(parameters);
-        }
-    }
+			// initialize IMU
+			BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+			parameters.mode				 = BNO055IMU.SensorMode.IMU;
+			parameters.angleUnit			= BNO055IMU.AngleUnit.RADIANS;
+			parameters.accelUnit			= BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+			parameters.loggingEnabled	   = false;
+			imu							 = hardwareMap.get(BNO055IMU.class, "imu");
+			imu.initialize(parameters);
+		}
+	}
 
-    @Override
-    public void loop() {
-        // Gets our values from gamepad one.
-        double xAxisMovement = gamepad1.left_stick_x;
-        double yAxisMovement = gamepad1.left_stick_y;
-        double rAxisMovement = gamepad1.right_stick_x;
-        // Gets our values from gamepad two.
-        double lAxisMovement = gamepad2.left_stick_y;
-        double cAxisMovement = gamepad2.right_stick_x;
+	@Override
+	public void loop() {
+		// Gets our values from gamepad one.
+		double xAxisMovement = gamepad1.left_stick_x;
+		double yAxisMovement = gamepad1.left_stick_y;
+		double rAxisMovement = gamepad1.right_stick_x;
+		// Gets our values from gamepad two.
+		double lAxisMovement = gamepad2.left_stick_y;
+		double cAxisMovement = gamepad2.right_stick_x;
 
-        Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.RADIANS);
-        double currentAngle = angles.firstAngle;
+		Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.RADIANS);
+		double currentAngle = angles.firstAngle;
 
-        // Handle gamepad1 inputs.
-        {
-            // Removes stick drift.
-            if (Math.abs(xAxisMovement) < joystick_max_drift) {
-                xAxisMovement = 0;
-            }
-            if (Math.abs(yAxisMovement) < joystick_max_drift) {
-                yAxisMovement = 0;
-            }
-            if (Math.abs(rAxisMovement) < joystick_max_drift) {
-                rAxisMovement = 0;
-            }
+		// Handle gamepad1 inputs.
+		{
+			// Removes stick drift.
+			if (Math.abs(xAxisMovement) < joystick_max_drift) {
+				xAxisMovement = 0;
+			}
+			if (Math.abs(yAxisMovement) < joystick_max_drift) {
+				yAxisMovement = 0;
+			}
+			if (Math.abs(rAxisMovement) < joystick_max_drift) {
+				rAxisMovement = 0;
+			}
 
-            // Conditional statement that increases drivepower once per buttonpress.
-            if (gamepad1.right_trigger == 1) {
-                if (right_trigger_pressed == false) {
-                    // Ensures that drive power does not exceed a value of 1.
-                    right_trigger_pressed = true;
-                    drive_power = Math.min(drive_power + drive_power_increment, 1);
-                }
-            } else {
-                right_trigger_pressed = false;
-            }
+			// Conditional statement that increases drivepower once per buttonpress.
+			if (gamepad1.right_trigger == 1) {
+				if (right_trigger_pressed == false) {
+					// Ensures that drive power does not exceed a value of 1.
+					right_trigger_pressed = true;
+					drive_power = Math.min(drive_power + drive_power_increment, 1);
+				}
+			} else {
+				right_trigger_pressed = false;
+			}
 
-            // Conditional statement that decreases drivepower once per buttonpress.
-            if (gamepad1.left_trigger == 1) {
-                if (left_trigger_pressed == false) {
-                    // Ensures that drive power does not go below a value of 0.1.
-                    left_trigger_pressed = true;
-                    drive_power = Math.max(drive_power - drive_power_increment, drive_min_power);
-                }
-            } else {
-                left_trigger_pressed = false;
-            }
-        }
+			// Conditional statement that decreases drivepower once per buttonpress.
+			if (gamepad1.left_trigger == 1) {
+				if (left_trigger_pressed == false) {
+					// Ensures that drive power does not go below a value of 0.1.
+					left_trigger_pressed = true;
+					drive_power = Math.max(drive_power - drive_power_increment, drive_min_power);
+				}
+			} else {
+				left_trigger_pressed = false;
+			}
+		}
 
-        {
-            double output = 0; // pidController.calculateOutput(currentAngle, elapsedTime.time());
-            
-            // Calculates power required to make motors move as needed.
-            double front_left_power   = yAxisMovement + xAxisMovement - rAxisMovement + output;
-            double front_right_power  = yAxisMovement - xAxisMovement + rAxisMovement - output;
-            double back_left_power    = yAxisMovement - xAxisMovement - rAxisMovement + output;
-            double back_right_power   = yAxisMovement + xAxisMovement + rAxisMovement - output;
+		{
+			double output = 0; // pidController.calculateOutput(currentAngle, elapsedTime.time());
+			
+			// Calculates power required to make motors move as needed.
+			double front_left_power   = yAxisMovement + xAxisMovement - rAxisMovement + output;
+			double front_right_power  = yAxisMovement - xAxisMovement + rAxisMovement - output;
+			double back_left_power	= yAxisMovement - xAxisMovement - rAxisMovement + output;
+			double back_right_power   = yAxisMovement + xAxisMovement + rAxisMovement - output;
 
-            // Applies drive power multiplier to increase/decrease speed.
-            front_left_power  *= drive_power;
-            front_right_power *= drive_power;
-            back_left_power   *= drive_power;
-            back_right_power  *= drive_power;
+			// Applies drive power multiplier to increase/decrease speed.
+			front_left_power  *= drive_power;
+			front_right_power *= drive_power;
+			back_left_power   *= drive_power;
+			back_right_power  *= drive_power;
 
-            // Send calculated power to wheels.
-            motors.frontLeft.setPower(front_left_power);
-            motors.frontRight.setPower(front_right_power);
-            motors.backLeft.setPower(back_left_power);
-            motors.backRight.setPower(back_right_power);
-        }
+			// Send calculated power to wheels.
+			motors.frontLeft.setPower(front_left_power);
+			motors.frontRight.setPower(front_right_power);
+			motors.backLeft.setPower(back_left_power);
+			motors.backRight.setPower(back_right_power);
+		}
 
-        double linear_slide_power = lAxisMovement;
-        // motors.linearSlide.setTargetPosition(10);
-        // motors.linearSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);    
-        motors.linearSlide.setPower(linear_slide_power * 0.5);
-        motors.claw.setPosition(cAxisMovement);
+		double linear_slide_power = lAxisMovement;
+		// motors.linearSlide.setTargetPosition(10);
+		// motors.linearSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);	
+		motors.linearSlide.setPower(linear_slide_power * 0.5);
+		// motors.claw.setPosition(cAxisMovement);
 
-        // motors.claw.setPosition(0);
-        // Handle gamepad2 inputs.
-        // {
-        //     motors.claw.scaleRange(claw_min_range, claw_max_range);
-        //     if (gamepad2.a) {
-        //         if (is_buttonA_pressed == false) {
-        //             is_buttonA_pressed = true;
-        //             telemetry.addData("pressed A", true);
-        //             motors.claw.setPosition(is_claw_toggled ? 0 : 1);
-        //             is_claw_toggled = !is_claw_toggled;
-        //         }
-        //     } else {
-        //         is_buttonA_pressed = false;
-        //     }
-        // }
+		// motors.claw.setPosition(0);
+		// Handle gamepad2 inputs.
+		{
+			if (gamepad2.a) {
+				if (is_buttonA_pressed == false) {
+					is_buttonA_pressed = true;
 
-        // Outputs out data.
-        {
-            telemetry.addData("linear_slide_position", motors.linearSlide.getCurrentPosition());
-            // telemetry.addLine();
-            // telemetry.addData("angle        :", currentAngle);
-            // telemetry.addData("PID          :", output);
-            // telemetry.addData("getError     :", pidController.getError());
-            // telemetry.addData("getErrorSum  :", pidController.getErrorSum());
-            // telemetry.addData("onTarget     :", pidController.onTarget());
-            // telemetry.addData("elapsed      :", elapsedTime.time());
-            // telemetry.addLine();
-            // telemetry.addData("drive_power   :", drive_power);
-            // telemetry.addData("frontLeft    :", motors.frontLeft.getPower());
-            // telemetry.addData("frontRight   :", motors.frontRight.getPower());
-            // telemetry.addData("backLeft     :", motors.backLeft.getPower());
-            // telemetry.addData("backRight    :", motors.backRight.getPower());
-            // telemetry.addLine();
-            // telemetry.addData("linearSlide  :", motors.linearSlide.getPower());
-            telemetry.addData("claw         :", motors.claw.getPosition());
-            // telemetry.addLine();
-            // telemetry.addData("xDistance    :", xAxisMovement);
-            // telemetry.addData("yDistance    :", yAxisMovement);
-            // telemetry.addData("xDistance2   :", rAxisMovement);
-            // telemetry.addData("yDistance2   :", "none");
-            telemetry.update();
-            elapsedTime.reset();
-        }
-    }
+					motors.claw.setPosition(is_claw_toggled ? claw_min_range : claw_max_range);
+					is_claw_toggled = !is_claw_toggled;
+				}
+			} else {
+				is_buttonA_pressed = false;
+			}
+			
+			telemetry.addData("pressed A", is_claw_toggled);
+		}
+
+		// Outputs out data.
+		{
+			telemetry.addData("linear_slide_position", motors.linearSlide.getCurrentPosition());
+			// telemetry.addLine();
+			// telemetry.addData("angle		:", currentAngle);
+			// telemetry.addData("PID		  :", output);
+			// telemetry.addData("getError	 :", pidController.getError());
+			// telemetry.addData("getErrorSum  :", pidController.getErrorSum());
+			// telemetry.addData("onTarget	 :", pidController.onTarget());
+			// telemetry.addData("elapsed	  :", elapsedTime.time());
+			// telemetry.addLine();
+			// telemetry.addData("drive_power   :", drive_power);
+			// telemetry.addData("frontLeft	:", motors.frontLeft.getPower());
+			// telemetry.addData("frontRight   :", motors.frontRight.getPower());
+			// telemetry.addData("backLeft	 :", motors.backLeft.getPower());
+			// telemetry.addData("backRight	:", motors.backRight.getPower());
+			// telemetry.addLine();
+			// telemetry.addData("linearSlide  :", motors.linearSlide.getPower());
+			telemetry.addData("claw		 :", motors.claw.getPosition());
+			// telemetry.addLine();
+			// telemetry.addData("xDistance	:", xAxisMovement);
+			// telemetry.addData("yDistance	:", yAxisMovement);
+			// telemetry.addData("xDistance2   :", rAxisMovement);
+			// telemetry.addData("yDistance2   :", "none");
+			telemetry.update();
+			elapsedTime.reset();
+		}
+	}
 }
